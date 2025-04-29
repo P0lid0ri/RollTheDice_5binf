d24f1134104e3dc27861cfac6298ba4fc0198730
P0lid0ri <gabrielescuola9910@gmail.com>
2025-04-29 12:42:08 +0200

    Primo upload

---
+++ app/src/main/java/com/example/rollthedice_5binf/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.rollthedice_5binf
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        var btnRoll =  findViewById<Button>(R.id.buttonRoll)
+        var text = findViewById<TextView>(R.id.textViewTitle)
+        var img = findViewById<ImageView>(R.id.imageViewDice)
+
+        btnRoll.setOnClickListener(View.OnClickListener {
+            var numero = estrazioneNumero()
+            var mioToast = Toast.makeText(this, "DADO LANCIATO !!! $numero", Toast.LENGTH_LONG)
+            mioToast.show()
+            lanciaIntent(numero)
+        })
+
+
+    }
+    private fun estrazioneNumero():Int {
+        return (1..6).random()
+    }
+    private fun lanciaIntent (numero:Int){
+        var intent = Intent(this,MainActivity2::class.java)
+        intent.putExtra("NUMERO" , numero)
+        startActivity(intent)
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/rollthedice_5binf/MainActivity2.kt
@@ -0,0 +1,65 @@
+package com.example.rollthedice_5binf
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+
+        val numeroEstratto = intent.getIntExtra("NUMERO", -1)
+        val textViewTitle = findViewById<TextView>(R.id.textView_numero)
+        textViewTitle.text = "${textViewTitle.text} $numeroEstratto"
+
+        val imageView = findViewById<ImageView>(R.id.image_dice)
+        imageView.setImageResource(
+            resources.getIdentifier("dice_face_$numeroEstratto", "drawable", packageName)
+        )
+        val resource = when (numeroEstratto){
+
+            1 -> R.drawable.dice_face_1
+            2 -> R.drawable.dice_face_2
+            3 -> R.drawable.dice_face_3
+            4 -> R.drawable.dice_face_4
+            5 -> R.drawable.dice_face_5
+            6 -> R.drawable.dice_face_6
+            else -> {R.drawable.dices}
+
+        }
+        imageView.setImageResource(resource)
+        val btnLancia = findViewById<Button>(R.id.button_win)
+
+        btnLancia.setOnClickListener(View.OnClickListener {
+            val mioToast = Toast.makeText(this, "Il Risultato è ..." ,Toast.LENGTH_LONG)
+            mioToast.show()
+            lanciasuIntent(numeroEstratto)
+        })
+    }
+    private fun estraiNumero():Int{
+        return (1..6).random()
+    }
+    private fun lanciasuIntent(numeroEstratto: Int,){
+        val intent = Intent(this, MainActivity3::class.java)
+        val numerogen= estraiNumero()
+        val esito:String
+        if(numeroEstratto == numerogen) {
+            esito = "Complimenti...Hai vinto!!!"
+        }else{
+            esito= "Mi Dispiace...Hai perso :( "
+        }
+        intent.putExtra("ESITO",esito)
+        intent.putExtra("ESTRAZIONE",numerogen)
+        startActivity(intent)
+    }
+}
